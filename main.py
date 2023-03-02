fbb6b47f4ba71e0652fea71ecb4426e26874c4a8
Albérico de Castro <alberico.castro@gethelix.com.br>
2023-03-02 19:52:16 -0300

    create func exibir & remove Hello World
    create func exibir & remove Hello World

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello Wolrd")
+dicionario = {1254:["Alberico", "Professor"]}
+
+def exibir():
+    for chave, lista in dicionario.items():
+        print("Cod. do Funcionario...", chave)
+        print("Nome..................", lista[0])
+        print("Cargo.................", lista[1])
