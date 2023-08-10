ef70e719c92321d2a2125c1470ffef52d04bfc32
Albérico de Castro <alberico.castro@gethelix.com.br>
2023-08-10 20:20:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,12 @@
 A module for demonstrating a dictionary and how to print its contents.
 """
 
-print("TESTE")
+dicionario = {1254: ["Alberico", "Professor"]}
+
+def exibir():
+    for chave, lista in dicionario.items():
+        print("Cod. do Funcionario...", chave)
+        print("Nome..................", lista[0])
+        print("Cargo.................", lista[1])
+
+exibir()
